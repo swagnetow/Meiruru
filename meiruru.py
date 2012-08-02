d789eb96c0d02b4fae3e926d8e6f06020f84905f
JP Villanueva <jpvi@ymail.com>
2012-08-02 04:42:50 -0700

    First commit.

---
+++ meiruru.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python2.7
+
+import socket
+
+s = socket.socket()
+host = "127.0.0.1"
+port = 2525
+
+s.connect((host, port))
+print s.recv(512)
+
+s.send("HELO localhost\r\n")
+print s.recv(512)
+
+s.send("MAIL FROM: <example@example.com>\r\n")
+print s.recv(512)
+
+s.send("RCPT TO: <example@example.com>\r\n")
+print s.recv(512)
+
+s.send("DATA\r\n")
+s.send("Subject: Hi!\r\n")
+s.send("\r\n")
+s.send("Bye!\r\n")
+print s.recv(512)
+
+s.send(".\r\n")
+print s.recv(512)
+
+s.send("QUIT\r\n")
+print s.recv(512)
+
+s.close
